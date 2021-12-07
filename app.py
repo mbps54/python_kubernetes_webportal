37efd5b8fd04f71a69ef9bd0eb30375e29926e47
mbps54 <utkin@posteo.net>
2021-12-07 15:04:06 +0300

    v1.0

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from doc1 import doc1
 import re
 
 def checkEmail(email):
-    #regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    regex = r'\b[A-Za-z0-9._%+-]+@akkuyu\.com\b'
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    #regex = r'\b[A-Za-z0-9._%+-]+@akkuyu\.com\b'
     if(re.fullmatch(regex, email)):
         return True
     else:
