98ce23577062d0382917014bec7fe708d60b06e0
mbps54 <utkin@posteo.net>
2021-12-23 12:01:50 +0300

    add currency.py

--- app/currency.py
+++ app/currency.py
@@ -5,8 +5,12 @@ import os
 def get_rates():
     response = requests.get("http://api.currencylayer.com/live?access_key=ca0cdd8c332da1840ec1e46a16ece708&source=USD&currencies=RUB,TRY&format=1")
     usdrub = ((response.json())['quotes'])['USDRUB']
-    usdtry = ((response.json())['quotes'])['USDRUB']
-    text_file = open('rates.yaml', "w")
+    usdtry = ((response.json())['quotes'])['USDTRY']
+    try:
+        os.makedirs(os.path.expanduser("../temp"))
+    except:
+        pass
+    text_file = open('../temp/rates.yaml', "w")
     n = text_file.write(f"---\n'usdrub': '{usdrub}'\n")
     n = text_file.write(f"'usdtry': '{usdtry}'\n...")
     text_file.close()
