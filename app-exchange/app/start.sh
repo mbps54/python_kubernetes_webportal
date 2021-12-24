c84efdbdc6c53e236a8e55982f22432981fe820d
mbps54 <utkin@posteo.net>
2021-12-24 22:06:32 +0300

    cron update

--- app-exchange/app/start.sh
+++ app-exchange/app/start.sh
@@ -6,20 +6,22 @@ ntpd -gq > /dev/null 2> /dev/null
 service ntp start > /dev/null 2> /dev/null
 echo "Current date and time:"
 date
-if [[ -z $POST_SCHEDULE ]]; then
+if [[ -z $CRON_SCHEDULE ]]; then
     echo "The script will executes only once"
 else
-    echo "he script will be scheduled: ${POST_SCHEDULE//X/*}"
+    echo "he script will be scheduled: ${CRON_SCHEDULE//X/*}"
 fi
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
-if [[ -z $POST_SCHEDULE ]]; then
-    exec python3 /app/app-exchange.py
+python3 /app/app-exchange.py
+
+if [[ -z $CRON_SCHEDULE ]]; then
+    echo "."
 else
     echo "SHELL=/bin/bash
     BASH_ENV=/container.env
-    ${POST_SCHEDULE//X/*} /app/app-exchange.py
+    ${CRON_SCHEDULE//X/*} /app/app-exchange.py
     # This extra line makes it a valid cron" > scheduler.txt
     crontab scheduler.txt
     exec cron -f
